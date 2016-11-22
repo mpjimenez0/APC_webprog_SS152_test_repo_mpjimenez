79129bd090330ca796d91d783c3bb31af2341f35
mpjimenez0 <cramjimenez07@gmail.com>
2016-11-22 22:22:09 +0800

    yay

--- Exercise-TryAgain/application/views/news/edit.php
+++ Exercise-TryAgain/application/views/news/edit.php
@@ -5,40 +5,38 @@
 <?php echo form_open('news/edit/'.$user_item['user_id']); ?>
     <table>
         <tr>
-            <td><label for="first_name">First Name</label></td>
+            <th><label for="first_name">First Name</label></th>
             <td><input type="input" name="first_name" size="50" value="<?php echo $user_item['first_name'] ?>" /></td>
         </tr>
         <tr>
-            <td><label for="last_name">Last Name</label></td>
+            <th><label for="last_name">Last Name</label></td>
             <td><input type="input" name="last_name" size="50" value="<?php echo $user_item['last_name'] ?>" /></td>
         </tr>
         <tr>
-            <td><label for="nickname">Nickname</label></td>
+            <th><label for="nickname">Nickname</label></th>
             <td><input type="input" name="nickname" size="50" value="<?php echo $user_item['nickname'] ?>" /></td>
         </tr>
         <tr>
-            <td><label for="email">E-mail</label></td>
+            <th><label for="email">E-mail</label></th>
             <td><input type="input" name="email" size="50" value="<?php echo $user_item['email'] ?>" /></td>
         </tr>
         <tr>
-            <td><label for="user_city">City</label></td>
+            <th><label for="user_city">City</label></th>
             <td><input type="input" name="user_city" size="50" value="<?php echo $user_item['user_city'] ?>" /></td>
         </tr>
         <tr>
-            <td><label for="gender">Gender</label></td>
+            <th><label for="gender">Gender</label></th>
             <td><input type="input" name="gender" size="50" value="<?php echo $user_item['gender'] ?>" /></td>
         </tr>
         <tr>
-            <td><label for="mobile">Mobile</label></td>
+            <th><label for="mobile">Mobile</label></th>
             <td><input type="input" name="Mobile" size="50" value="<?php echo $user_item['mobile'] ?>" /></td>
         </tr>
         <tr>
-            <td><label for="comment">Comment</label></td>
+            <th><label for="comment">Comment</label></th>
             <td><textarea name="comment" rows="3" cols="40"><?php echo $user_item['comment'] ?></textarea></td>
         </tr>
         <tr>
-            <td></td>
-            <td><input type="submit" name="submit" value="Edit news item" /></td>
         </tr>
     </table>
 </form>
