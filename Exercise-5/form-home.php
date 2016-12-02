19826fe312bbb0c0351be30a685d314ebbcdfaf2
mpjimenez0 <cramjimenez07@gmail.com>
2016-12-02 10:51:48 +0800

    fixed the sql of previous exercises (5)

--- Exercise-5/form-home.php
+++ Exercise-5/form-home.php
@@ -14,7 +14,166 @@ if(isset($_GET['delete_id']))
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>
 		WEBPROG-Excercise5
-	</title
+	</title>
+  <style>
+    body{
+      background-image: url("clouds-bg.png");
+      background-attachment: fixed;
+      font-family: quicksand;
+    }
+    h1{
+      text-align:center;
+      color:black;
+    }
+    b{
+      font-style: strong;
+    }
+    div{
+      text-align:center;
+    }
+    a:link{
+      text-decoration:none;
+      color:black;
+    }
+    a:visited{
+      color: black;
+    }
+    a:hover, a:active{
+      color: #966fd6;
+    }
+    p{
+      text-align: center;
+      font-size: 30;
+    }
+    table {
+      font-family: quicksand;
+      width: 100%;
+      font-size: 25;
+      padding-left: 70;
+      padding-right: 70;
+    }
+    th{
+      background:#FFD1DC;
+    }
+    td, th {
+      border: 3px solid #000000;
+      text-align: left;
+      padding: 10px;
+    }
+    td{
+      background-color: rgba(255,255,255,.5);
+    }
+    hr{
+      width: 58%;
+      color: black;
+    }
+    #div-pic{
+      width: 120;
+      height: 120;
+    }
+    #normal{
+      font-size: 25;
+      padding-left:10%;
+      padding-right:10%;
+    }
+    #subcat{
+      display:inline-block;
+      border:1px;
+      padding:5px 0;
+    }
+    #extramenu{
+      width: 100%;
+      height: 10%;
+    }
+    #title{
+      font-size: 85;
+      height: 40;
+      padding: 20;
+    }
+    #just{
+      font-size: 25;
+      padding-left:10%;
+      padding-right:10%;
+      text-align: justify;
+    }
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+    }
+
+    #div.menu{
+      width:465px;
+      margin: 0 auto;
+      border:1px solid red;
+      text-align:center;
+    }
+    #menu{
+      margin: 0 auto;
+      text-align: center;
+    }
+
+    li {
+      float: left;
+    }
+
+    li a, .dropbtn {
+      display: inline-block;
+      color: white;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+    }
+    li.dropdown {
+      display: inline-block;
+    }
+
+    .dropdown-content {
+      display: none;
+      position: absolute;
+      min-width: 110px;
+      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+      background-color: rgba(232,232,232,0.9);
+    }
+
+    .dropdown-content a {
+      color: black;
+      margin-top: 2;
+      text-decoration: none;
+      display: block;
+      text-align: center;
+    }
+
+    .dropdown-content a:hover {
+      color: #660066;
+    }
+
+    .dropdown:hover .dropdown-content {
+      display: block;
+    }
+    @font-face {
+      font-family: quicksand;
+      src: url(quicksand_book.otf);
+    }
+    @font-face {
+      font-family: amethyst;
+      src: url(shorelines.otf);
+    }
+    div.transbox {
+      background-color: rgba(255,255,255,.5);
+      margin: 30px;
+      margin-left: 600px;
+      margin-right: 600px;
+      padding:10;
+    }
+    .error {
+      color: #FF0000;
+    }
+    input[type=text]:focus{
+      width: 50%;
+    }
+  </style>
 	<script type="text/javascript">
 		function edt_id(id)
 		{
