075887cef521f52ea69e1c60b04f8dcfa9b48a51
mpjimenez0 <cramjimenez07@gmail.com>
2016-12-26 23:09:05 +0800

    added randomize button to create new set of answers

--- Exercise-7/basic/views/trivias/index.php
+++ Exercise-7/basic/views/trivias/index.php
@@ -15,14 +15,17 @@ $(document).ready(function()
        $('answer').toggle();
     });
 });
+function reload() {
+    location.reload();
+}
 </script>
 
 <h1>Trivias</h1>
-<p>listed below are 5 random trivias</p>
+<p style="font-size:1.5em">listed below are 5 random trivias</p>
 <ul>
 <?php foreach ($trivia as $trivia): ?>
 
-    <li>
+    <li style="font-size:1.2em">
         <strong><?= Html::encode("{$trivia->triv_q}") ?>:</strong>
 		<br>
 		<answer style="display:none"> -- &nbsp;<?=$trivia->triv_ans ?></answer>
@@ -31,5 +34,6 @@ $(document).ready(function()
     </li>
 <?php endforeach; ?>
 
-<button id="show">Show all answers</button>
+<button id="show">Show all answers</button> <br><br>
+<button onclick="reload()">RANDOMIZE !</button>
 </ul>
